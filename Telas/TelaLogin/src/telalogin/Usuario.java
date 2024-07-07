c473f9ef669a899cc5496c9689e14323a6931153
Mathenos2077 <156183065+Mathenos2077@users.noreply.github.com>
2024-07-07 01:07:50 -0300

    Adição de telas
    Adição das telas de login e de agendamento de serviços pelo cliente.

---
+++ Telas/TelaLogin/src/telalogin/Usuario.java
@@ -0,0 +1,31 @@
+package telalogin;
+
+/**
+ *  FXMLTelaLogin Model Class
+ * 
+ * @author matheus teixeira pires
+ */
+public class Usuario {
+    private String usuario, senha;
+
+    public Usuario() {
+    }
+    public Usuario(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
