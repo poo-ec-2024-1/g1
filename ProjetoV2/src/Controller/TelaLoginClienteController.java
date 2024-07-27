d5f4470a02f016fae99884610deafc9716809dd8
JoãoPedroSantos <147274947+JoaoPSantos0@users.noreply.github.com>
2024-07-27 17:31:57 -0300

    Add files via upload

--- ProjetoV2/src/Controller/TelaLoginClienteController.java
+++ ProjetoV2/src/Controller/TelaLoginClienteController.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,19 +10,44 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class TelaLoginClienteController {
     private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private Button BotaoEntrar;
+    @FXML
+    private Button BotaoRegistrar;
+    
+    public void switchToMenu(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/View/TelaMenuPrinc.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
     @FXML
-    public void onClickEntrar(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("/View/TelaMenuPrinc.fxml"));
-        Scene scene = new Scene(root);
+    public void switchToRegistro(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/View/TelaRegistrarCliente.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void switchToEmpresaSide(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/Empresa/TelaLoginEmpresa.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 }
+    
+    
+   
--- ProjetoV2/src/Controller/TelaRegistrarClienteController.java
+++ ProjetoV2/src/Controller/TelaRegistrarClienteController.java
@@ -1,26 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
+
 package Controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author joaopedro
- */
-public class TelaRegistrarClienteController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+public class TelaRegistrarClienteController  {
+
+    @FXML
+    private Button BotaoEntrar;
+    @FXML
+    private Button BotaoEntrar1;
+    private Parent root;
+    private Scene scena;
+    private Stage stage;
+
+    @FXML
+    private void onClickVoltar(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/TelaLoginCliente.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scena = new Scene(root);
+        stage.setScene(scena);
+        stage.show();
+    }
     
+    
+
+   
 }
