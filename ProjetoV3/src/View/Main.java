6202b89cf1d62304951538c4706a29c3429133ba
JoãoPedroSantos <147274947+JoaoPSantos0@users.noreply.github.com>
2024-07-31 14:51:46 -0300

    Add files via upload

---
+++ ProjetoV3/src/View/Main.java
@@ -0,0 +1,35 @@
+
+package View;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) {
+        
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("TelaLoginCliente.fxml"));
+            Scene scene = new Scene(root);
+            //stage.initStyle(StageStyle.UNDECORATED);
+            stage.setTitle("Car Wash");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
