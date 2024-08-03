8701a8673d7a04f4965cc831cb55b5bbb3042d62
JoãoPedroSantos <147274947+JoaoPSantos0@users.noreply.github.com>
2024-08-03 16:06:23 -0300

    Alteraçao de telas e datas

---
+++ ProjetoV3/src/Model/ClienteRepository.java
@@ -0,0 +1,152 @@
+package Model;
+
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.dao.Dao;
+import java.sql.SQLException;
+import com.j256.ormlite.table.TableUtils;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *  Repositório para o banco de dados dos clientes
+ * 
+ * @author Matheus TP
+ */
+
+public class ClienteRepository{
+    private static Database database;
+    private static Dao<Cliente, Integer> dao;
+    private List<Cliente> loadedServicos;
+    private Cliente loadedCliente; 
+    // Construtores
+    public ClienteRepository(Database database) {
+        ClienteRepository.setDatabase(database);
+        loadedServicos = new ArrayList<Cliente>();
+    }
+    /**
+     * Seleciona o banco de dados sobre qual o repositorio vai operar
+     * 
+     * @param database Banco de dados que deseja atribuir ao repositório 
+     */
+    public static void setDatabase(Database database) {
+        ClienteRepository.database = database;
+        try {
+            dao = DaoManager.createDao(database.getConnection(), Cliente.class);
+            TableUtils.createTableIfNotExists(database.getConnection(), Cliente.class);
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+        }            
+    }
+    /**
+     *  Adiciona uma nova instância de Cliente ao banco de dados
+     * 
+     * @param cliente Instância do tipo Cliente
+     * @return Retorna o mesmo cliente que foi dado no parâmetro, porém com o seu ID definido
+     */
+    public Cliente create(Cliente cliente) {
+        int nrows = 0;
+        try {
+            nrows = dao.create(cliente);
+            if ( nrows == 0 )
+                throw new SQLException("Error: object not saved");
+            this.loadedCliente = cliente;
+            loadedServicos.add(cliente);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return cliente;
+    }    
+    /**
+     * Atualiza o Cliente existente no banco de dados que possui o mesmo ID que o do parâmetro
+     * 
+     * @param cliente Instância do tipo Cliente
+     */
+    public void update(Cliente cliente) {
+      try{
+          dao.update(cliente);
+      }catch (SQLException e){
+          System.out.println("Não foi possível executar a atualização.");
+      }
+    }
+    /**
+     * Deleta o Cliente existente no banco de dados que possui o mesmo ID que o do parâmetro
+     * 
+     * @param cliente Instância do tipo Cliente
+     */
+    public void delete(Cliente cliente) {
+      try{
+          dao.delete(cliente);
+      } catch (SQLException e){
+          System.out.println("Não foi possível executar a exclusão.");
+      }
+    }
+    /**
+     * Deleta o Cliente existente no banco de dados que possui o ID passado no parâmetro
+     * 
+     * @param id ID para ser deletado
+     */
+    public void deleteById(int id){
+        try{
+            dao.deleteById(id);
+        } catch (SQLException e){
+            System.out.println("Não foi possível executar a exclusão.");
+        }
+    }
+    /**
+     * Carrega o cliente do banco de dados que possui o ID passado no parâmetro
+     * 
+     * @param id ID para ser carregado
+     * @return Retorna esse cliente
+     */
+    public Cliente loadFromId(int id) {
+        try {
+            this.loadedCliente = dao.queryForId(id);
+            if (this.loadedCliente != null)
+                this.loadedServicos.add(this.loadedCliente);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return this.loadedCliente;
+    }    
+    /**
+     * 
+     * @return Uma lista com todos os clientes contidos no banco de dados
+     */
+    public List<Cliente> loadAll() {
+        try {
+            this.loadedServicos =  dao.queryForAll();
+            if (this.loadedServicos.size() != 0)
+                this.loadedCliente = this.loadedServicos.get(0);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return this.loadedServicos;
+    }
+    /**
+     * Método responsável por validar se um cliente existe no banco de dados através de seu usuario e senha, e além disso,
+     * obtêm o seu id.
+     * 
+     * @param usuario que deseja autenticar
+     * @param senha que deseja autenticar
+     * @return retorna o id do cliente se ele existir no banco de dados. Do contrário, retorna 0;
+     */
+    public int autenticarCliente(String email, String senha){
+        int id = 0;
+        try{
+            List<Cliente> clientes = this.loadAll();
+            for(int i = clientes.size(); i>0; i--){
+                String emailDatabase = clientes.get(i-1).getEmail();
+                String senhaDatabase = clientes.get(i-1).getSenha();
+                if(emailDatabase.equals(email) && senhaDatabase.equals(senha)){
+                        id = clientes.get(i-1).getId();
+                        break;
+                    }
+                }
+            } catch (Exception e){
+            System.out.println("Não foi possível realizar a autenticação");
+        }
+        return id;
+    }
+    // getters and setters ommited...        
+}
---
+++ ProjetoV3/src/Model/ServicoRepository.java
@@ -0,0 +1,127 @@
+package Model;
+
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.dao.Dao;
+import java.sql.SQLException;
+import com.j256.ormlite.table.TableUtils;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *  Repositório para o banco de dados dos servicos
+ * 
+ * @author Matheus TP
+ */
+
+public class ServicoRepository{
+    private static Database database;
+    private static Dao<Servico, Integer> dao;
+    private List<Servico> loadedServicos;
+    private Servico loadedServico; 
+    // Construtores
+    public ServicoRepository(Database database) {
+        ServicoRepository.setDatabase(database);
+        loadedServicos = new ArrayList<Servico>();
+    }
+    /**
+     * Seleciona o banco de dados sobre qual o repositorio vai operar
+     * 
+     * @param database Banco de dados que deseja atribuir ao repositório 
+     */
+    public static void setDatabase(Database database) {
+        ServicoRepository.database = database;
+        try {
+            dao = DaoManager.createDao(database.getConnection(), Servico.class);
+            TableUtils.createTableIfNotExists(database.getConnection(), Servico.class);
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+        }            
+    }
+    /**
+     *  Adiciona uma nova instância de Servico ao banco de dados
+     * 
+     * @param servico Instância do tipo Servico
+     * @return Retorna o mesmo Servico que foi dado no parâmetro, porém com o seu ID definido
+     */
+    public Servico create(Servico servico) {
+        int nrows = 0;
+        try {
+            nrows = dao.create(servico);
+            if ( nrows == 0 )
+                throw new SQLException("Error: object not saved");
+            this.loadedServico = servico;
+            loadedServicos.add(servico);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return servico;
+    }    
+    /**
+     * Atualiza o Servico existente no banco de dados que possui o mesmo ID que o do parâmetro
+     * 
+     * @param servico Instância do tipo Servico
+     */
+    public void update(Servico servico) {
+      try{
+          dao.update(servico);
+      }catch (SQLException e){
+          System.out.println("Não foi possível executar a atualização.");
+      }
+    }
+    /**
+     * Deleta o Servico existente no banco de dados que possui o mesmo ID que o do parâmetro
+     * 
+     * @param servico Instância do tipo Servico
+     */
+    public void delete(Servico servico) {
+      try{
+          dao.delete(servico);
+      } catch (SQLException e){
+          System.out.println("Não foi possível executar a exclusão.");
+      }
+    }
+    /**
+     * Deleta o Servico existente no banco de dados que possui o ID passado no parâmetro
+     * 
+     * @param id ID para ser deletado
+     */
+    public void deleteById(int id){
+        try{
+            dao.deleteById(id);
+        } catch (SQLException e){
+            System.out.println("Não foi possível executar a exclusão.");
+        }
+    }
+    /**
+     * Carrega o servico do banco de dados que possui o ID passado no parâmetro
+     * 
+     * @param id ID para ser carregado
+     * @return Retorna esse servico
+     */
+    public Servico loadFromId(int id) {
+        try {
+            this.loadedServico = dao.queryForId(id);
+            if (this.loadedServico != null)
+                this.loadedServicos.add(this.loadedServico);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return this.loadedServico;
+    }    
+    /**
+     * 
+     * @return Uma lista com todos os Servicos contidos no banco de dados
+     */
+    public List<Servico> loadAll() {
+        try {
+            this.loadedServicos =  dao.queryForAll();
+            if (this.loadedServicos.size() != 0)
+                this.loadedServico = this.loadedServicos.get(0);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return this.loadedServicos;
+    }
+    // getters and setters ommited...        
+}
