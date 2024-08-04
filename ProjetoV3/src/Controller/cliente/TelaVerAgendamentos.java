d62393c97cfc1781a151171bb7589f771214f03b
Matheus Teixeira Pires <matheus.teteu.tp.72@gmail.com>
2024-08-03 23:52:48 -0300

    "Atualização. Agora os clientes podem ver os agendamentos que fez"

--- ProjetoV3/src/Controller/cliente/TelaVerAgendamentos.java
+++ ProjetoV3/src/Controller/cliente/TelaVerAgendamentos.java
@@ -1,20 +1,72 @@
 
 package Controller.cliente;
 
+import Model.model.Agendamento;
+import Model.repository.AgendamentoRepository;
+import Model.repository.Database;
+import Util.*;
+import static Util.ExibirAgendamento.getAgendamentoExibivel;
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-public class TelaVerAgendamentos {
+public class TelaVerAgendamentos implements Initializable{
     private Stage stage;
     private Scene scene;
     private Parent root;
     
+    @FXML
+    private Button BotaoVoltar;
+    @FXML
+    private TableColumn<AgendamentoExibivel, String> colData;
+    @FXML
+    private TableColumn<AgendamentoExibivel, String> colEmpresa;
+    @FXML
+    private TableColumn<AgendamentoExibivel, String> colEndereco;
+    @FXML
+    private TableColumn<AgendamentoExibivel, String> colHora;
+    @FXML
+    private TableColumn<AgendamentoExibivel, Integer> colId;
+    @FXML
+    private TableColumn<AgendamentoExibivel, String> colPreco;
+    @FXML
+    private TableColumn<AgendamentoExibivel, String> colServico;
+    @FXML
+    private TableColumn<AgendamentoExibivel, String> colVeiculo;
+    @FXML
+    private TableView<AgendamentoExibivel> tabel;
     
+    ObservableList<AgendamentoExibivel> agends;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        agends = FXCollections.observableArrayList();
+        Database database = Util.openDatabase("agendamentosDatabase");
+        AgendamentoRepository agendamentoRP = new AgendamentoRepository(database);
+        List<Agendamento> agendamentos = agendamentoRP.loadAll();
+        for(Agendamento x : agendamentos){
+            if(x.getCliente() == TelaLoginClienteController.idSelecionado){
+                agends.add(getAgendamentoExibivel(x));
+            }
+        }
+        carregarDados();
+    }
+        
     public void onClickVoltar(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("/View/TelaMenuPrinc.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -23,4 +75,16 @@ public class TelaVerAgendamentos {
         stage.show();
     }
     
+    public void carregarDados(){
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colServico.setCellValueFactory(new PropertyValueFactory<>("servico"));
+        colEmpresa.setCellValueFactory(new PropertyValueFactory<>("empresa"));
+        colPreco.setCellValueFactory(new PropertyValueFactory<>("preco"));
+        colVeiculo.setCellValueFactory(new PropertyValueFactory<>("veiculo"));
+        colData.setCellValueFactory(new PropertyValueFactory<>("data"));
+        colHora.setCellValueFactory(new PropertyValueFactory<>("hora"));
+        colEndereco.setCellValueFactory(new PropertyValueFactory<>("endereco"));
+        
+        tabel.setItems(agends);
+    }
 }
---
+++ ProjetoV3/src/Util/AgendamentoExibivel.java
@@ -0,0 +1,94 @@
+package Util;
+
+
+public class AgendamentoExibivel {
+    
+    private int id;
+    private String servico;
+    private String empresa;
+    private String preco;
+    private String veiculo;
+    private String data;
+    private String hora;
+    private String endereco;
+
+    public AgendamentoExibivel() {
+    }
+
+    public AgendamentoExibivel(int id, String servico, String empresa, String preco, String veiculo, String data, String hora, String endereco) {
+        this.id = id;
+        this.servico = servico;
+        this.empresa = empresa;
+        this.preco = preco;
+        this.veiculo = veiculo;
+        this.data = data;
+        this.hora = hora;
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getServico() {
+        return servico;
+    }
+
+    public void setServico(String servico) {
+        this.servico = servico;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(String veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+}
